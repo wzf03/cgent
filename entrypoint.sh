6e0ff0d84f7552fd2f2898f9eb800a60eb94657d
Zhuofeng Wang <35804928+wzf03@users.noreply.github.com>
2025-05-19 11:01:53 +0800

    feat: Add more default config

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,10 @@ client_secret: ${SECRET}
 server: '${SERVER}'
 tls: ${TLS}
 uuid: ${UUID}
+disable_auto_update: true
+disable_command_execute: true
+disable_force_update: true
+skip_procs_count: true
 EOF
 fi
 exec /cgent -c="$CONFIG_FILE"
